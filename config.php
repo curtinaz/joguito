81d1fc544429f409f78d6596551c5856f6c9e8d0
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-08 22:26:29 -0300

    Update config.php

--- config.php
+++ config.php
@@ -14,8 +14,8 @@ try{
 
 $username = 'laura';
 
-$stmt = $conexao->prepare("INSERT INTO `tb_user` (`user_id`, `user_name`) VALUES (NULL, ':username')");
-$stmt->execute(array('username' => $username));
+$stmt = $conexao->prepare("INSERT INTO `tb_user` (`user_id`, `user_name`) VALUES (NULL, :username)");
+$stmt->execute(array('username' => $username)); // palavra reservada, com :username, sem aspas.
 
 //primeira vez que eu conecto com PDO.
 
