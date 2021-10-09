215f5e3fa0cd375be36f8d499c444eda01859c7b
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-08 22:11:53 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ $db = 'joguito';
 $username = 'root';
 $password = '';
 
-$conn = new PDO('mysql:host=localhost;dbname=meuBancoDeDados', $username, $password);
+$conexao = new PDO('mysql:host=' . $host . ';dbname=meuBancoDeDados', $username, $password);
 
 ?>
