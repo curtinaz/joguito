1addefe1abfc47ee69c5a8d5d45b936a01ed9be7
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-09 22:43:32 -0300

    comentários removidos

--- config.php
+++ config.php
@@ -7,19 +7,4 @@ $db = 'joguito';
 
 $con = new mysqli($host, $username, $password, $db);
 
-// try{
-//     $conexao = new PDO('mysql:host=' . $host . ';dbname=' . $db, $username, $password);
-//     echo 'conectou';
-// } catch(PDOException $e) {
-//     echo 'ERROR: ' . $e->getMessage();
-// }
-
-// $username = 'teste';
-
-// $stmt = $conexao->prepare("INSERT INTO `tb_user` (`user_id`, `user_name`) VALUES (NULL, :username)");
-// $stmt->execute(array('username' => $username)); // palavra reservada, com :username, sem aspas.
-// $stmt = NULL;
-
-//primeira vez que eu conecto com PDO.
-
 ?>
