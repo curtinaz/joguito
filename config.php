31705c9b72af477468bf7f46e1219384a2354463
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-10-09 22:42:43 -0300

    A conexão, agora se chama $con

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $username = 'root';
 $password = '';
 $db = 'joguito';
 
-$conexao = new mysqli($host, $username, $password, $db);
+$con = new mysqli($host, $username, $password, $db);
 
 // try{
 //     $conexao = new PDO('mysql:host=' . $host . ';dbname=' . $db, $username, $password);
